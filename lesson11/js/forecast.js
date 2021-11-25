d48ad13bcb31ef725d12918ce581f3bfc65d3986
stephanyzamora <85659956+stephanyzamora@users.noreply.github.com>
2021-11-25 01:28:12 -0500

    Add files via upload

---
+++ lesson11/js/forecast.js
@@ -0,0 +1,23 @@
+let cityID = 5604473
+const apiForecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=c1dcfb6c8efcda936d6f6f66d54711d8&units=imperial";
+
+fetch(apiForecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
+
+    const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    let newList = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+    
+    for (let day = 0; day <= 4; day ++) {
+        let d = new Date(newList[day].dt_txt);
+        document.getElementById(`dayWeek${day+1}`).textContent = dayOfWeek[d.getDay()];
+        document.getElementById(`forecast${day+1}`).textContent = newList[day].main.temp;
+
+        const imgalt = newList[day].weather[0].description;
+        const imagesrc = 'https://openweathermap.org/img/wn/' + newList[day].weather[0].icon + '@2x.png';
+        document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+        document.getElementById(`icon${day+1}`).setAttribute('alt', imgalt);
+    }
+
+});
