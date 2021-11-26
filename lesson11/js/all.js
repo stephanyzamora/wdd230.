4c0afe91b9adcc909326eb8ac9a4f0b39c4d4954
stephanyzamora <85659956+stephanyzamora@users.noreply.github.com>
2021-11-25 20:37:17 -0500

    Update all.js

--- lesson11/js/all.js
+++ lesson11/js/all.js
@@ -3,7 +3,7 @@ const mainMenu = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () =>{mainMenu.classList.toggle('responsive')}, false);
 
-window.onresize = () => {
+window.onresize = () =>{
     if(window.innerWidth > 760){
         mainMenu.classList.remove('responsive');
     }
